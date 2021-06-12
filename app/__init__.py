eb436f8e039c221f8147c0118ee76e59d0440e33
Caleb Odinga <calemasanga@gmail.com>
2021-06-12 17:27:14 +0300

    created a function that returns an app with config_name as a parameter

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
 
+def create_app(config_name):
 
-#intializing app
-app = Flask(__name__)
+  #intializing app
+  app = Flask(__name__)
+
+  #creating app configurations
+  app.config.from_object(config_options[config_name])
+
+  #Initializing flask extensions
+  bootstrap.init_app(app)  
+
+  from .main import views, forms, errors
+
+  return app
