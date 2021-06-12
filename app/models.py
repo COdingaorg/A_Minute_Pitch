307223d57a166fe4cbce34e7394754d53f520aba
Caleb Odinga <calemasanga@gmail.com>
2021-06-12 19:11:21 +0300

    adds a comment class in models file

--- app/models.py
+++ app/models.py
@@ -11,3 +11,13 @@ class Pitch(db.Model):
   votes = db.Column(db.Integer, db.ForeignKey('votes.id'))
   postedBy = db.Column(db.String, db.ForeignKey('users.id'))
 
+class Comment(db.Model):
+  '''
+  Defines arguments for pitch comments instances and stores them in database
+  '''
+  __tablename__='comments'
+  id = db.Column(db.Integer, primary_key = True)
+  content = db.Column(db.String(255))
+  postedBy = db.Column(db.String(255), ForeignKey('users.id'))
+  datePosted = db.Column(db.DateTime, default = datetime.utcnow)
+
