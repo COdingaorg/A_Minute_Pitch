0945601b64ad79f749aa6a5a866a8622c6385728
Caleb Odinga <calemasanga@gmail.com>
2021-06-14 10:51:57 +0300

    adds email validator package, adds length validators on pssword,adds register.html

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
-from wtforms.validators import Email, Required, EqualTo
+from wtforms.validators import Email, Required, EqualTo, Length
 from ..models import User
 
 class SignupForm(FlaskForm):
   username = StringField('Your Name: ', validators=[Required()])
   email = StringField('Email Address: ', validators=[Required(), Email()])
-  password = PasswordField('Create password: ', validators=[Required(), EqualTo('password_confirm', message='Passwords must be the same')])
+  password = PasswordField('Create password: ', validators=[Length(min=6, max=16),Required(), EqualTo('password_confirm', message='Passwords must be the same')])
   password_confirm = PasswordField('Repeat Password: ', validators=[Required()])
   signup = SubmitField('signup')
 
@@ -18,4 +18,3 @@ class SignupForm(FlaskForm):
   def validate_email(self, data_field):
     if User.query.filter_by(email=data_field.data).first():
       raise ValidationError('Email already exists')
-      
