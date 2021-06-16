632eec853595aed8307a6b222532a1dbf34fad16
Caleb Odinga <calemasanga@gmail.com>
2021-06-16 22:57:09 +0300

    modifies start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ export SECRET_KEY='immutable'
 export MAIL_USERNAME='caleb.odinga@student.moringaschool.com'
 export MAIL_PASSWORD='1lov3h3r@MS'
 
-python3.8 manage.py server
+python3.8 manage.py runserver
