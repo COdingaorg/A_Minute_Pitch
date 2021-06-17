ea240ced4e95da5b0e40c299396081ae5a37ca66
Caleb Odinga <calemasanga@gmail.com>
2021-06-17 21:09:14 +0300

    adds Pitch category to manage

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from app.models import Comment, Pitch, User, Vote
+from app.models import Comment, Pitch, User, Vote, PitchCategory
 
 #creating app instance
 app = create_app('production')
@@ -14,7 +14,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-  return dict(app = app, db = db, User = User, Pitch = Pitch, Vote = Vote, Comment = Comment)
+  return dict(app = app, db = db, User = User, Pitch = Pitch, Vote = Vote, Comment = Comment, PitchCategory = PitchCategory)
 
 if __name__=='__main__':
   manager.run()
